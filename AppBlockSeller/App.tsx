75bdc06e37084aa03626f0a07277d18ba9a7cd45
German Arbelaez <german.arbelaez@xpertgroup.co>
2023-05-05 16:39:36 -0500

    feat: :construction: Create folder structure

---
+++ AppBlockSeller/App.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import ScreenLogin from './src/screens/ScreenLogin';
+
+const App = () => {
+  return <ScreenLogin />;
+};
+
+export default App;
---
+++ AppBlockSeller/src/screens/ScreenLogin.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {styles} from '../theme/appTheme';
+
+const ScreenLogin = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ScreenLogin</Text>
+    </View>
+  );
+};
+
+export default ScreenLogin;
